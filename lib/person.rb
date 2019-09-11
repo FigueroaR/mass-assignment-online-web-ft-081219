6dc7b85c81dce8fcdfcf89471b5019fffcc5f6e4
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-11 19:01:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :hair, :handed, :height, :weight
+  attr_accessor :name, :hair, :handed, :height, :weight, :birthday
  
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}

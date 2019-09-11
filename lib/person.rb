8d20bea3bfcd39c9b06cd26e09583ffbaa92315e
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-11 19:19:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :hair_color, :handed, :height, :weight, :birthday, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :hair_color, :handed, :height, :weight, :birthday, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width, :eye_color
  
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
